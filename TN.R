3ab47d2b5c164558d2a509d2aaf2246280751a16
itsamywithaj <55413512+itsamywithaj@users.noreply.github.com>
2020-06-17 16:39:12 -0400

    Add files via upload

---
+++ TN.R
@@ -0,0 +1,117 @@
+rm(list = ls()) # clear working environment
+setwd("~/Downloads/States/")
+install.packages("tidyverse") # install tidyverse
+library(tidyverse)
+install.packages("readxl") # CRAN version
+library(readxl)
+
+#-------  Enrollment --------
+tn_enrl <- read_excel("raw_data/TN_201819_membership.xlsx") 
+# dataset downloaded from https://www.tn.gov/content/dam/tn/education/data/201819_membership.xlsx
+names(tn_enrl) <- tolower(names(tn_enrl))
+names(tn_enrl) <- c("dist_num","district","school_num","school","grade","race","gender","count")
+summary(tn_enrl)
+tn_enrl$district <- as.factor(tn_enrl$district)
+tn_enrl$school <- as.factor(tn_enrl$school)
+tn_enrl$grade <- as.factor(tn_enrl$grade)
+tn_enrl$race <- as.factor(tn_enrl$race)
+tn_enrl$gender <- as.factor(tn_enrl$gender)
+# pare down to just Nashville, all grades
+tn_enrl <- tn_enrl %>% 
+  filter(district == "Metro Nashville Public Schools",
+         grade == "All Grades") %>% 
+  select(district, school, race, gender, count) %>% 
+  arrange(district, school)
+tn_enrl <- tn_enrl %>% 
+  distinct(school, district, gender, race, .keep_all = TRUE) %>% 
+  spread(key = race,
+         value = count,
+         fill = 0) %>% 
+  distinct(school, district, gender, .keep_all = TRUE) %>% 
+  spread(key = gender,
+         value = `All Race/Ethnic Groups`,
+         fill = 0)
+names(tn_enrl)[3:10] <- c("count_amind","count_asian","count_black","count_hispanic","count_multiple","count_hawpi","count_white","total") 
+# recode Hawaii and PI to match other members
+tn_enrl<-tn_enrl %>% 
+  mutate(count_asian = count_asian + count_hawpi,
+         amind = count_amind/total,
+         asian = count_asian/total,
+         black = count_black/total,
+         hispanic = count_hispanic/total,
+         white = count_white/total,
+         multiple = count_multiple/total) %>%
+  select(district, school, total, amind, asian, black, hispanic, white, multiple) %>% 
+  filter(total>0)
+tn_stan_enroll <- tn_enrl %>% 
+  filter(school != "Valor Flagship Academy" &
+           school != "Valor Voyager Academy")
+tn_stan_enroll <- tn_stan_enroll %>% 
+  summarize(mean_amind = mean(amind),
+            mean_asian = mean(asian),
+            mean_black = mean(black),
+            mean_hispanic = mean(hispanic),
+            mean_white = mean(white),
+            mean_multiple = mean(multiple),
+            sd_amind = sd(amind),
+            sd_asian = sd(asian),
+            sd_black = sd(black),
+            sd_hispanic = sd(hispanic),
+            sd_white = sd(white),
+            sd_multiple = sd(multiple)) %>% 
+  mutate(district = "Metro Nashville Public Schools") %>% 
+  select(district, mean_amind,sd_amind, mean_asian, sd_asian, mean_black, sd_black, mean_hispanic, sd_hispanic,
+         mean_white, sd_white, mean_multiple, sd_multiple)
+valor <- tn_enrl %>% 
+  filter(school == "Valor Flagship Academy"|
+           school == "Valor Voyager Academy")
+valor <- merge(valor, tn_stan_enroll, by="district")
+valor <- valor %>% 
+  mutate(comp_amind = (amind - mean_amind)/sd_amind,
+         comp_asian = (asian - mean_asian)/sd_asian,
+         comp_black = (black - mean_black)/sd_black,
+         comp_hispanic = (hispanic - mean_hispanic)/sd_hispanic,
+         comp_white = (white - mean_white)/sd_white,
+         comp_multiple = (multiple - mean_multiple)/sd_multiple) %>% 
+  select(school,total, amind, mean_amind, comp_amind,
+         asian, mean_asian, comp_asian,
+         black, mean_black, comp_black,
+         hispanic, mean_hispanic, comp_hispanic,
+         white, mean_white, comp_white,
+         multiple, mean_multiple, comp_multiple)
+
+write.csv(valor, file = file.path('output_data/tn_stan_enroll.csv'), row.names = FALSE)
+
+
+#-------  ELL --------
+tn_ell <- read.csv("raw_data/TN_wida_growth_standard_school_suppressed.csv") 
+# dataset downloaded from https://www.tn.gov/content/dam/tn/education/accountability/2019/wida_growth_standard_school_suppressed.csv
+summary(tn_ell)
+names(tn_ell)[1:6] <- c("sys_numb","system","sch_numb","school","subgroup","count")
+tn_ell <- tn_ell %>% 
+  filter(subgroup=="All Students") %>% 
+  select(sys_numb,sch_numb, school, count) %>% 
+  arrange(sys_numb, sch_numb)
+district_ell <- tn_enrl %>% 
+  select(school, total) # list of schools and total enrollment in Metro Nashville PS District
+merger <- tn_ell[tn_ell$school %in% district_ell$school ,]
+district_ell <- merge(district_ell, merger)
+district_ell <- district_ell %>% 
+  select(school, total, count) %>% 
+  mutate(perc_ell = count/total)
+
+dist_ell <- district_ell %>% 
+  filter(school != "Valor Flagship Academy"&
+           school!="Valor Voyager Academy")
+tn_stan_ell <- district_ell %>% 
+  summarize(mean_ell = mean(perc_ell),
+            sd_ell = sd(perc_ell))
+valor_ell <- district_ell %>% 
+  filter(school == "Valor Flagship Academy"|
+           school == "Valor Voyager Academy") %>% 
+  mutate(mean_ell = tn_stan_ell$mean_ell,
+         sd_ell = tn_stan_ell$sd_ell,
+         comp_ell = (perc_ell - mean_ell)/sd_ell) %>% 
+  select(school, total, perc_ell, mean_ell, comp_ell)
+
+write.csv(valor_ell, file = file.path("output_data/tn_stan_ell.csv"), row.names = FALSE)
